74de883d8805c59b495800557157fbb73e2c409d
BN-2K9 <bn2k9mc@gmail.com>
2024-06-20 19:46:44 +0200

    Added the Config And Other Features

--- src/main/java/me/bn_2k9/darkskies/DarkSkies.java
+++ src/main/java/me/bn_2k9/darkskies/DarkSkies.java
@@ -1,8 +1,11 @@
 package me.bn_2k9.darkskies;
 
+import me.bn_2k9.darkskies.Methods.Backpack;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,6 +15,9 @@ public final class DarkSkies extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        p.saveDefaultConfig();
+
         Bukkit.getLogger().info("Stared!");
 
     }
@@ -26,8 +32,19 @@ public final class DarkSkies extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender instanceof Player) {
+            if (command.getName().equals("Backpack")) {
+
+                Backpack.openBackpack(sender.getServer().getPlayer(sender.getName()));
+
+            }
+        }
 
         return true;
+    }
+
+    public static String Colorcode (String s) {
+        return ChatColor.translateAlternateColorCodes('&', s);
     }
 
 }
--- src/main/java/me/bn_2k9/darkskies/Methods/Backpack.java
+++ src/main/java/me/bn_2k9/darkskies/Methods/Backpack.java
@@ -1,11 +1,20 @@
 package me.bn_2k9.darkskies.Methods;
 
+import me.bn_2k9.darkskies.DarkSkies;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 public class Backpack {
+    static Plugin pl = DarkSkies.getPlugin(DarkSkies.class);
+    static String prefix = DarkSkies.Colorcode(pl.getConfig().getString("Plugin.Prefix"));
+    public static void openBackpack(Player p) {
 
-    public void openBackpack(Player p) {
-
+        if (p.getInventory().getItemInOffHand().getType() == Material.PAPER && p.getInventory().getItemInOffHand().getItemMeta().getCustomModelData() ==  pl.getConfig().getInt("Features.Backpack.CustomModelData")){
+            p.openInventory(p.getEnderChest());
+        } else {
+            p.sendMessage(prefix + DarkSkies.Colorcode(pl.getConfig().getString("Messages:")));
+        }
 
 
     }
