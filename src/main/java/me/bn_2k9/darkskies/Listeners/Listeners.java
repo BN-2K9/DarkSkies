493732a311014e5c2ac380f8f1314de264af6f90
BN-2K9 <bn2k9mc@gmail.com>
2024-06-21 18:39:36 +0200

    Before trying the plugin

--- src/main/java/me/bn_2k9/darkskies/Listeners/Listeners.java
+++ src/main/java/me/bn_2k9/darkskies/Listeners/Listeners.java
@@ -1,13 +1,16 @@
 package me.bn_2k9.darkskies.Listeners;
 
 import me.bn_2k9.darkskies.DarkSkies;
+import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 
 import java.util.List;
+import java.util.Random;
 
 public class Listeners implements Listener {
 
@@ -23,12 +26,23 @@ public class Listeners implements Listener {
 
                     if (e.getPlayer().getInventory().getItemInMainHand().getItemMeta().hasCustomModelData()) {
                         if (e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getCustomModelData() == pl.getConfig().getInt("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".CustomModelData")) {
-
                            if (pl.getConfig().contains("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".LootTable"))) {
                                 List Loot = pl.getConfig().getList("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".LootTable"));
+                                if (Loot.isEmpty()) {
 
+                                    Random ran = new Random();
+                                    int value = ran.nextInt(Loot.size());
 
+                                    String mat = Loot.get(value).toString().toUpperCase();
+                                    ItemStack Item = new ItemStack(Material.valueOf(mat));
 
+                                    if (pl.getConfig().getString("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".LootTable") + "."+ mat + ".ItemName") != null) {
+                                        Item.getItemMeta().setDisplayName(pl.getConfig().getString("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".LootTable") + "."+ mat + ".ItemName"));
+                                    }
+                                    if (pl.getConfig().getInt("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".LootTable") + "."+ mat + ".CustomModelData") != 0) {
+                                        Item.getItemMeta().setCustomModelData(pl.getConfig().getInt("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".LootTable") + "."+ mat + ".ItemName"));
+                                    }
+                                }
                            }
 
                         }
