8c69c627a68921558a463808e21a27b2d7f7e837
BN-2K9 <bn2k9mc@gmail.com>
2024-06-22 19:05:44 +0200

    Forgot a long time to update

--- src/main/java/me/bn_2k9/darkskies/Listeners/Listeners.java
+++ src/main/java/me/bn_2k9/darkskies/Listeners/Listeners.java
@@ -1,12 +1,15 @@
 package me.bn_2k9.darkskies.Listeners;
 
 import me.bn_2k9.darkskies.DarkSkies;
+import org.apache.commons.lang.WordUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 
 import java.util.List;
@@ -18,49 +21,79 @@ public class Listeners implements Listener {
     @EventHandler
     public void OnPlayerFish(PlayerFishEvent e) {
 
+        String nameone = e.getPlayer().getInventory().getItemInMainHand().getType().name().toLowerCase().replace("_", " ");
+        String Finished = WordUtils.capitalizeFully(nameone);
+        Bukkit.getLogger().info(Finished);
+
         if (e.getCaught() instanceof Item) {
 
-            if (pl.getConfig().contains("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName()) ) {
+            if (pl.getConfig().contains("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().hasItemMeta()) ) {
+
+                if (pl.getConfig().contains("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().hasDisplayName()) ) {
+                    Finished = pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName());
+                }
 
                 if (pl.getConfig().getInt("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".CustomModelData")  != -1) {
 
                     if (e.getPlayer().getInventory().getItemInMainHand().getItemMeta().hasCustomModelData()) {
-                        if (e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getCustomModelData() == pl.getConfig().getInt("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".CustomModelData")) {
-                           if (pl.getConfig().contains("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".LootTable"))) {
-                                List Loot = pl.getConfig().getList("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".LootTable"));
-                                if (Loot.isEmpty()) {
-
-                                    Random ran = new Random();
-                                    int value = ran.nextInt(Loot.size());
-
-                                    String mat = Loot.get(value).toString().toUpperCase();
-                                    ItemStack Item = new ItemStack(Material.valueOf(mat));
-
-                                    if (pl.getConfig().getString("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".LootTable") + "."+ mat + ".ItemName") != null) {
-                                        Item.getItemMeta().setDisplayName(pl.getConfig().getString("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".LootTable") + "."+ mat + ".ItemName"));
-                                    }
-                                    if (pl.getConfig().getInt("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".LootTable") + "."+ mat + ".CustomModelData") != 0) {
-                                        Item.getItemMeta().setCustomModelData(pl.getConfig().getInt("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getDisplayName() + ".LootTable") + "."+ mat + ".ItemName"));
-                                    }
-                                }
-                           }
+                        if (e.getPlayer().getInventory().getItemInMainHand().getItemMeta().getCustomModelData() == pl.getConfig().getInt("Features.Fishing.Tools." + Finished + ".CustomModelData")) {
+                            if (pl.getConfig().contains("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + Finished + ".LootTable"))) {
+                                GiveLoot(e, Finished);
+                                e.getHook().remove();
+                                e.setCancelled(true);
+                            }
 
                         }
 
                     }
-                } else {
-
                 }
-
-
+            } else {
+                GiveLoot(e, Finished);
+                e.getHook().remove();
+                e.setCancelled(true);
             }
 
         }
 
     }
 
-    public static void GiveLoot() {
+    public static void GiveLoot(PlayerFishEvent e, String s) {
 
+        Bukkit.getLogger().info(pl.getConfig().getString("Features.Fishing.Tools." + s + ".LootTable"));
+        List Loot = pl.getConfig().getList("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + s + ".LootTable"));
+        if (Loot != null) {
+
+            Random ran = new Random();
+            int value = ran.nextInt(Loot.size());
+
+            String Num = Loot.get(value).toString().replace("{", "").replace("=", " ").split(" ")[0];
+            String mat = pl.getConfig().getString("Features.Fishing.LootTables.Common." + Num + ".Material");
+
+            Bukkit.getLogger().info("Features.Fishing.LootTables.Common." + Num + ".Material");
+            Bukkit.getLogger().info(Num);
+            Bukkit.getLogger().info(mat);
+
+            ItemStack Item = new ItemStack(Material.valueOf(mat));
+
+            if (pl.getConfig().getString("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + s + ".LootTable") + "."+ Num + ".ItemName") != null) {
+                Bukkit.getLogger().info("Added Name");
+                ItemMeta Im = Item.getItemMeta();
+
+                Im.setDisplayName(pl.getConfig().getString("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + s + ".LootTable") + "."+ Num + ".ItemName"));
+
+                Item.setItemMeta(Im);
+            }
+            if (pl.getConfig().getInt("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + s + ".LootTable") + "."+ Num + ".CustomModelData") != -1) {
+                Bukkit.getLogger().info("Added data");
+                ItemMeta Im = Item.getItemMeta();
+                Im.setCustomModelData(pl.getConfig().getInt("Features.Fishing.LootTables." + pl.getConfig().getString("Features.Fishing.Tools." + s + ".LootTable") + "." + Num + ".ItemName"));
+
+                Item.setItemMeta(Im);
+            }
+
+            e.getPlayer().getInventory().addItem(Item);
+
+        }
     }
 
 }
